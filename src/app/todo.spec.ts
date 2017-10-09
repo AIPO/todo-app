1d4dca386f00e8b0fd0212a2c32b68c34a756d59
povilas <p.vaitonis@gmail.com>
2017-10-09 14:34:15 +0300

    test: added Todo class test for constructor

--- src/app/todo.spec.ts
+++ src/app/todo.spec.ts
@@ -1,7 +1,16 @@
-import { Todo } from './todo';
+import {Todo} from './todo';
 
 describe('Todo', () => {
-  it('should create an instance', () => {
-    expect(new Todo()).toBeTruthy();
-  });
+    it('should create an instance', () => {
+        expect(new Todo()).toBeTruthy();
+    });
+    it('should accept values in the constructor', () => {
+        let todo = new Todo({
+            title: 'Test task',
+            complete: true
+        });
+        expect(todo.title).toEqual('Test task');
+        expect(todo.complete).toEqual(true);
+    });
+
 });
